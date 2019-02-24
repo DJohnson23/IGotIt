c7bdd2421bd408fdef204126a64f53069ba4c591
Gregory Johnson <gsoccer111@gmail.com>
2019-02-24 09:29:38 -0500

    Java Backend
    Starter files for backend

---
+++ Java/IGotIt/src/main/java/com/api/IGotIt/IGotItApplication.java
@@ -0,0 +1,13 @@
+package com.api.IGotIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IGotItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IGotItApplication.class, args);
+	}
+
+}
